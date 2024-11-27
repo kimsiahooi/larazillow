9afd42b52854b9e7f9d46451f37a35fcd4dab25f
Ooi Kim Siah <kimsiahooi@gmail.com>
2024-11-27 20:20:51 +0800

    36

--- database/migrations/2024_11_27_012450_create_listings_table.php
+++ database/migrations/2024_11_27_012450_create_listings_table.php
@@ -14,6 +14,16 @@ return new class extends Migration
         Schema::create('listings', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->unsignedTinyInteger('beds');
+            $table->unsignedTinyInteger('baths');
+            $table->unsignedTinyInteger('area');
+
+            $table->tinyText('city');
+            $table->tinyText('code');
+            $table->tinyText('street');
+            $table->tinyText('street_nr');
+
+            $table->unsignedInteger('price');
         });
     }
 
