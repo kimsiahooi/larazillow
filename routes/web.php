cbfef3d5803a160621b72a04d3fdda05328da5ad
Ooi Kim Siah <kimsiahooi@gmail.com>
2024-11-29 13:11:18 +0800

    78

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,10 @@ use App\Http\Controllers\ListingController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [IndexController::class, 'index']);
-Route::get('/hello', [IndexController::class, 'show']);
+Route::get('/hello', [IndexController::class, 'show'])->middleware('auth');
 
-Route::resource('listing', ListingController::class);
+Route::resource('listing', ListingController::class)->except(['create', 'store', 'edit', 'update', 'destroy']);
+Route::resource('listing', ListingController::class)->only(['create', 'store', 'edit', 'update', 'destroy'])->middleware(('auth'));
 
 Route::get('login', [AuthContoller::class, 'create'])->name('login');
 Route::post('login', [AuthContoller::class, 'store'])->name('login.store');
