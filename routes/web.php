a1d1515d7a7f04c274790e87587663c01aba26a7
Ooi Kim Siah <kimsiahooi@gmail.com>
2024-11-29 10:36:54 +0800

    70

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthContoller;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ListingController;
 use Illuminate\Support\Facades\Route;
@@ -8,3 +9,7 @@ Route::get('/', [IndexController::class, 'index']);
 Route::get('/hello', [IndexController::class, 'show']);
 
 Route::resource('listing', ListingController::class);
+
+Route::get('login', [AuthContoller::class, 'create'])->name('login');
+Route::post('login', [AuthContoller::class, 'store'])->name('login.store');
+Route::post('logout', [AuthContoller::class, 'destroy'])->name('logout');
