7290ded2e9555c8e6e20eb3e857e3c636444acc9
Ooi Pei Yen <yen@Oois-MBP.realtek>
2025-02-02 20:30:44 +0800

    42

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\ListingController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return inertia('Index/Index');
 });
+
+Route::resource('listing', ListingController::class)->only(['index', 'show']);
