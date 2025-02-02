55af45d31b8c5435c2d0ae4a81a2358bc06b5f3f
Ooi Pei Yen <yen@Oois-MBP.realtek>
2025-02-02 10:06:09 +0800

    36

--- database/migrations/2025_02_02_015650_create_listings_table.php
+++ database/migrations/2025_02_02_015650_create_listings_table.php
@@ -13,6 +13,14 @@ return new class extends Migration
     {
         Schema::create('listings', function (Blueprint $table) {
             $table->id();
+            $table->unsignedTinyInteger('beds');
+            $table->unsignedTinyInteger('baths');
+            $table->unsignedTinyInteger('area');
+            $table->tinyText('city');
+            $table->tinyText('code');
+            $table->tinyText('street');
+            $table->tinyText('street_nr');
+            $table->unsignedInteger('price');
             $table->timestamps();
         });
     }
