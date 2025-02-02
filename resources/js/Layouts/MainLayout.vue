d246236c42d4ef9d9248a63b49a983617425cf30
Ooi Pei Yen <yen@Oois-MBP.realtek>
2025-02-02 21:21:22 +0800

    47

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,8 +1,24 @@
 <template>
-    <div>
-        layout
-    </div>
+    <Link href="/">Main Page</Link>
+    <Link href="/hello">Show Page</Link>
+    <div v-if="flashSuccess" class="success">{{ flashSuccess }}</div>
     <main>
         <slot />
     </main>
 </template>
+
+<script setup>
+import { Link, usePage } from '@inertiajs/vue3';
+import { computed } from 'vue';
+
+const page = usePage();
+
+const flashSuccess = computed(() => page.props.flash.success);
+</script>
+
+<style scoped>
+.success {
+    background-color: green;
+    color: white;
+}
+</style>
