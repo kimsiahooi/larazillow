aa9079d38a9c63891f3b59d24f2bb8e6ba6a0446
Ooi Pei Yen <yen@Oois-MBP.realtek>
2025-02-02 20:58:10 +0800

    44

--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,5 @@ Route::get('/', function () {
     return inertia('Index/Index');
 });
 
-Route::resource('listing', ListingController::class)->only(['index', 'show']);
+Route::resource('listing', ListingController::class)
+    ->only(['index', 'show', 'create']);
